1a0d983504611653ed53600933f4e83ed3f4d747
caternuson <caternuson@gmail.com>
2018-11-01 15:25:46 -0700

    update example with SPI

--- examples/bmp3xx_simpletest.py
+++ examples/bmp3xx_simpletest.py
@@ -3,9 +3,15 @@ import board
 import busio
 import adafruit_bmp3xx
 
+# I2C setup
 i2c = busio.I2C(board.SCL, board.SDA)
-
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+
+# SPI setup
+# from digitalio import DigitalInOut, Direction
+# spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+# cs = DigitalInOut(board.D5)
+# bmp = adafruit_bmp3xx.BMP3XX_SPI(spi, cs)
 
 bmp.pressure_oversampling = 8
 bmp.temperature_oversampling = 2
