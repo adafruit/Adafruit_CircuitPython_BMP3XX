d2289fa263e317d2dcc43501cf3319ddb9029d7d
Alec Delaney <tekktrik@gmail.com>
2022-03-29 16:40:49 -0400

    Reformatted per new black version

--- adafruit_bmp3xx.py
+++ adafruit_bmp3xx.py
@@ -159,19 +159,19 @@ class BMP3XX:
         P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11 = self._pressure_calib
 
         pd1 = P6 * temperature
-        pd2 = P7 * temperature ** 2.0
-        pd3 = P8 * temperature ** 3.0
+        pd2 = P7 * temperature**2.0
+        pd3 = P8 * temperature**3.0
         po1 = P5 + pd1 + pd2 + pd3
 
         pd1 = P2 * temperature
-        pd2 = P3 * temperature ** 2.0
-        pd3 = P4 * temperature ** 3.0
+        pd2 = P3 * temperature**2.0
+        pd3 = P4 * temperature**3.0
         po2 = adc_p * (P1 + pd1 + pd2 + pd3)
 
-        pd1 = adc_p ** 2.0
+        pd1 = adc_p**2.0
         pd2 = P9 + P10 * temperature
         pd3 = pd1 * pd2
-        pd4 = pd3 + P11 * adc_p ** 3.0
+        pd4 = pd3 + P11 * adc_p**3.0
 
         pressure = po1 + po2 + pd4
 
@@ -187,22 +187,22 @@ class BMP3XX:
         # Note: forcing float math to prevent issues with boards that
         #       do not support long ints for 2**<large int>
         self._temp_calib = (
-            coeff[0] / 2 ** -8.0,  # T1
-            coeff[1] / 2 ** 30.0,  # T2
-            coeff[2] / 2 ** 48.0,
+            coeff[0] / 2**-8.0,  # T1
+            coeff[1] / 2**30.0,  # T2
+            coeff[2] / 2**48.0,
         )  # T3
         self._pressure_calib = (
-            (coeff[3] - 2 ** 14.0) / 2 ** 20.0,  # P1
-            (coeff[4] - 2 ** 14.0) / 2 ** 29.0,  # P2
-            coeff[5] / 2 ** 32.0,  # P3
-            coeff[6] / 2 ** 37.0,  # P4
-            coeff[7] / 2 ** -3.0,  # P5
-            coeff[8] / 2 ** 6.0,  # P6
-            coeff[9] / 2 ** 8.0,  # P7
-            coeff[10] / 2 ** 15.0,  # P8
-            coeff[11] / 2 ** 48.0,  # P9
-            coeff[12] / 2 ** 48.0,  # P10
-            coeff[13] / 2 ** 65.0,
+            (coeff[3] - 2**14.0) / 2**20.0,  # P1
+            (coeff[4] - 2**14.0) / 2**29.0,  # P2
+            coeff[5] / 2**32.0,  # P3
+            coeff[6] / 2**37.0,  # P4
+            coeff[7] / 2**-3.0,  # P5
+            coeff[8] / 2**6.0,  # P6
+            coeff[9] / 2**8.0,  # P7
+            coeff[10] / 2**15.0,  # P8
+            coeff[11] / 2**48.0,  # P9
+            coeff[12] / 2**48.0,  # P10
+            coeff[13] / 2**65.0,
         )  # P11
 
     def _read_byte(self, register):
