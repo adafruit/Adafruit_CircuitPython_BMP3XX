6a56ad79563e91746b742002952e20cd7c50a41a
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_bmp3xx.py
+++ adafruit_bmp3xx.py
@@ -32,7 +32,7 @@ import time
 
 from micropython import const
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BMP3XX.git"
 
 _BMP388_CHIP_ID = const(0x50)
